bb5632de1a9f8162da282447bdfdfc8702d72d82
howardmann <howardmann27@gmail.com>
2017-08-27 17:06:59 +1000

    Initial commit for sentiment-client setup project

---
+++ sentiment-client/server.js
@@ -0,0 +1,42 @@
+// Dependencies
+let express = require('express');
+let exphbs = require('express-handlebars');
+let path = require('path');
+let app = express();
+
+// Set view engine
+app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
+
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+// App
+app.get('/', function (req, res, next) {
+  res.render('home');
+});
+
+// Catch and send error messages
+app.use(function (err, req, res, next) {
+  if (err) {
+    res.status(422).json({
+      error: err.message
+    });
+  } else {
+    next();
+  }
+});
+
+// 404
+app.use(function (req, res) {
+  res.status(404).json({
+    status: 'Page does not exist'
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+// Expose and listen
+app.listen(PORT, function () {
+  console.log(`Listening to port ${PORT}`);
+});
+
+module.exports = app;
