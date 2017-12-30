1ef44a4d34007396d98595e1cd5645119c707312
howardmann <howardmann27@gmail.com>
2017-12-30 23:05:04 +1100

    Add coin

---
+++ client/test/data/prices/coinmarketcap/tickerParsed.js
@@ -0,0 +1,241 @@
+module.exports = [{
+    ticker: 'BTC',
+    currency: 'USD',
+    price: 13586.3,
+    mktcap: 227860932162,
+    changePctDay: -0.0647,
+    changePctWeek: -0.081
+  },
+  {
+    ticker: 'XRP',
+    currency: 'USD',
+    price: 2.48095,
+    mktcap: 96109881408,
+    changePctDay: 0.41200000000000003,
+    changePctWeek: 1.1444
+  },
+  {
+    ticker: 'ETH',
+    currency: 'USD',
+    price: 723.354,
+    mktcap: 69919802582,
+    changePctDay: -0.0207,
+    changePctWeek: -0.0062
+  },
+  {
+    ticker: 'BCH',
+    currency: 'USD',
+    price: 2578.48,
+    mktcap: 43534862934,
+    changePctDay: 0.0558,
+    changePctWeek: -0.1607
+  },
+  {
+    ticker: 'ADA',
+    currency: 'USD',
+    price: 0.65203,
+    mktcap: 16905227803,
+    changePctDay: 0.4597,
+    changePctWeek: 0.5342
+  },
+  {
+    ticker: 'LTC',
+    currency: 'USD',
+    price: 227.368,
+    mktcap: 12399953417,
+    changePctDay: -0.060599999999999994,
+    changePctWeek: -0.2126
+  },
+  {
+    ticker: 'MIOTA',
+    currency: 'USD',
+    price: 3.40414,
+    mktcap: 9461910218,
+    changePctDay: -0.0435,
+    changePctWeek: -0.149
+  },
+  {
+    ticker: 'XEM',
+    currency: 'USD',
+    price: 0.971397,
+    mktcap: 8742572999,
+    changePctDay: 0.0516,
+    changePctWeek: 0.0748
+  },
+  {
+    ticker: 'DASH',
+    currency: 'USD',
+    price: 1051.27,
+    mktcap: 8182980346,
+    changePctDay: -0.036000000000000004,
+    changePctWeek: -0.16690000000000002
+  },
+  {
+    ticker: 'XLM',
+    currency: 'USD',
+    price: 0.351988,
+    mktcap: 6286139354,
+    changePctDay: 0.4325,
+    changePctWeek: 0.507
+  },
+  {
+    ticker: 'XMR',
+    currency: 'USD',
+    price: 352.015,
+    mktcap: 5470878517,
+    changePctDay: -0.0333,
+    changePctWeek: -0.0924
+  },
+  {
+    ticker: 'EOS',
+    currency: 'USD',
+    price: 9.12961,
+    mktcap: 5214843084,
+    changePctDay: -0.022099999999999998,
+    changePctWeek: 0.019299999999999998
+  },
+  {
+    ticker: 'QTUM',
+    currency: 'USD',
+    price: 61.7641,
+    mktcap: 4556375457,
+    changePctDay: 0.0892,
+    changePctWeek: 0.1355
+  },
+  {
+    ticker: 'BTG',
+    currency: 'USD',
+    price: 266.812,
+    mktcap: 4465208503,
+    changePctDay: -0.015,
+    changePctWeek: -0.1863
+  },
+  {
+    ticker: 'NEO',
+    currency: 'USD',
+    price: 68.1642,
+    mktcap: 4430673000,
+    changePctDay: 0.0572,
+    changePctWeek: 0.0898
+  },
+  {
+    ticker: 'ETC',
+    currency: 'USD',
+    price: 28.6709,
+    mktcap: 2831178207,
+    changePctDay: 0.003,
+    changePctWeek: -0.1413
+  },
+  {
+    ticker: 'BCC',
+    currency: 'USD',
+    price: 414.172,
+    mktcap: 2562085819,
+    changePctDay: -0.0665,
+    changePctWeek: 0.36369999999999997
+  },
+  {
+    ticker: 'LSK',
+    currency: 'USD',
+    price: 20.0087,
+    mktcap: 2330453787,
+    changePctDay: -0.09949999999999999,
+    changePctWeek: -0.0518
+  },
+  {
+    ticker: 'TRX',
+    currency: 'USD',
+    price: 0.0336676,
+    mktcap: 2213583845,
+    changePctDay: -0.056600000000000004,
+    changePctWeek: -0.1814
+  },
+  {
+    ticker: 'XVG',
+    currency: 'USD',
+    price: 0.147544,
+    mktcap: 2125883067,
+    changePctDay: 0.019,
+    changePctWeek: -0.223
+  },
+  {
+    ticker: 'ICX',
+    currency: 'USD',
+    price: 5.2069,
+    mktcap: 1962367907,
+    changePctDay: -0.0279,
+    changePctWeek: 0.2891
+  },
+  {
+    ticker: 'BTS',
+    currency: 'USD',
+    price: 0.63368,
+    mktcap: 1651566521,
+    changePctDay: 0.0678,
+    changePctWeek: 0.1793
+  },
+  {
+    ticker: 'ARDR',
+    currency: 'USD',
+    price: 1.53633,
+    mktcap: 1534792894,
+    changePctDay: -0.0833,
+    changePctWeek: 0.2503
+  },
+  {
+    ticker: 'ZEC',
+    currency: 'USD',
+    price: 517.904,
+    mktcap: 1526143323,
+    changePctDay: 0.053399999999999996,
+    changePctWeek: -0.1023
+  },
+  {
+    ticker: 'XRB',
+    currency: 'USD',
+    price: 11.1548,
+    mktcap: 1486358017,
+    changePctDay: -0.2939,
+    changePctWeek: 1.5094
+  },
+  {
+    ticker: 'OMG',
+    currency: 'USD',
+    price: 14.335,
+    mktcap: 1462779980,
+    changePctDay: -0.0434,
+    changePctWeek: -0.0163
+  },
+  {
+    ticker: 'USDT',
+    currency: 'USD',
+    price: 1.03023,
+    mktcap: 1409447192,
+    changePctDay: 0.024,
+    changePctWeek: -0.0029
+  },
+  {
+    ticker: 'PPT',
+    currency: 'USD',
+    price: 36.3555,
+    mktcap: 1345299900,
+    changePctDay: -0.0404,
+    changePctWeek: -0.0815
+  },
+  {
+    ticker: 'STRAT',
+    currency: 'USD',
+    price: 13.5179,
+    mktcap: 1333957392,
+    changePctDay: -0.1005,
+    changePctWeek: 0.1905
+  },
+  {
+    ticker: 'WAVES',
+    currency: 'USD',
+    price: 12.1802,
+    mktcap: 1218020000,
+    changePctDay: -0.0404,
+    changePctWeek: -0.1454
+  }
+]
---
+++ client/test/src/prices/coin.spec.js
@@ -0,0 +1,53 @@
+// Dependencies
+let chai = require('chai');
+let expect = chai.expect;
+let _ = require('lodash')
+
+// Modules and test data
+let coin = require('../../../src/prices/coin');
+let priceMultiData = require('../../data/prices/coinmarketcap/ticker.json');
+let priceMultiParsed = require('../../data/prices/coinmarketcap/tickerParsed')
+
+describe('#coin', () => {
+  describe('.parseSingleTicker', () => {
+    it('should return relevant fields from a ticker', () => {
+      let BTC = priceMultiData[0];
+      let input = coin.parseSingleTicker(BTC)
+      let actual = {
+        ticker: 'BTC',
+        currency: 'USD',
+        price: 13586.3,
+        mktcap: 227860932162,
+        changePctDay: -0.0647,
+        changePctWeek: -0.081
+      }
+      expect(input).to.eql(actual)
+    })
+  })
+  describe('.parseTickers', () => {
+    it('should transform the payload to an array of parsed tickers sorted by marketcap', () => {
+      let data = priceMultiData
+      let input = coin.parseTickers(data)
+      let actual = priceMultiParsed
+      expect(input).to.eql(actual)
+    })
+  })
+  describe('.fetchTickers', () => {
+    it('should fetch and transform tickers', (done) => {
+      let axiosStub = {
+        get: function () {
+          return Promise.resolve({ data: priceMultiData })
+        }
+      }
+      coin.fetchTickers(axiosStub)
+        .then(result => {
+          let input = result
+          let actual = priceMultiParsed
+          expect(input).to.eql(actual)
+          done()
+        })
+    })
+  })
+})
+
+
