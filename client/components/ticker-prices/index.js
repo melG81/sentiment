22e506823a20b10bbb98c1e017fd988b2ca434dc
howardmann <howardmann27@gmail.com>
2017-12-30 23:20:55 +1100

    Update ticker prices to include weekly

--- client/components/ticker-prices/index.js
+++ client/components/ticker-prices/index.js
@@ -1,13 +1,13 @@
 // Dependencies
-let { fetchTickers } = require('../../src/prices')
-let tickerArr = require('../../src/filters/tickers.js')
+let { fetchTickers } = require('../../src/prices/coin')
+// let tickerArr = require('../../src/filters/tickers.js')
 
 var template = require('./template.hbs')
 
 let makeTickerPrices = function () {
   this.init = () => {
     let $tickerPrices = $('#ticker-prices')
-    fetchTickers(tickerArr).then(prices => {
+    fetchTickers().then(prices => {
       var html = template({ tickers: prices })
       $tickerPrices.html(html)      
     })
