bfc9a6e3f3ac077e4a8ae65ced599f3bf027b2d1
howardmann <howardmann27@gmail.com>
2018-02-04 22:27:12 +1100

    Sort comments by created

--- client/routes/threads.js
+++ client/routes/threads.js
@@ -2,17 +2,22 @@ let threads = module.exports = {}
 
 // Dependencies
 let dbClient = require('../src/util/dbClient')
+
 let _ = require('lodash')
 let { parseHtml } = require('../src/util/helpers')
 let moment = require('moment');
 
 threads.show = function (req, res, next) {
   let id = req.params.id
+  
   let user = _.get(req, 'user')
   let isAdmin = _.get(req, 'user.admin') && req.isAuthenticated()
   dbClient.getDoc(id)
     .then(payload => {
       let data = payload.data
+      let comments = _.get(data, 'comments')
+      data.comments = _.sortBy(comments, 'createdAt').reverse()
+
       res.render('threads/show', {
         data,
         user,
