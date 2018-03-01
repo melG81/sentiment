85bb0f16a53c6885edd228eeab8e94ac125b5cde
howardmann <howardmann27@gmail.com>
2018-03-01 22:50:25 +1100

    Messy need to fix

--- client/routes/threads.js
+++ client/routes/threads.js
@@ -6,6 +6,8 @@ let dbClient = require('../src/util/dbClient')
 let _ = require('lodash')
 let { parseHtml } = require('../src/util/helpers')
 let moment = require('moment');
+let flattenNested = require('../src/comments').flattenNested;
+
 
 threads.show = function (req, res, next) {
   let id = req.params.id
@@ -16,7 +18,7 @@ threads.show = function (req, res, next) {
     .then(payload => {
       let data = payload.data
       let comments = _.get(data, 'comments')
-      data.comments = _.sortBy(comments, 'createdAt').reverse()
+      data.comments = flattenNested(comments)
 
       res.render('threads/show', {
         data,
