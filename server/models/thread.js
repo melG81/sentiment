0c5832a47fcc0b1fa414a66d38c24afccadb0cca
howardmann <howardmann27@gmail.com>
2018-04-29 14:30:13 +1000

    Add schema for sentiment with defaults

--- server/models/thread.js
+++ server/models/thread.js
@@ -11,7 +11,16 @@ var CommentSchema = require('./comment.js');
 var Schema = new mongoose.Schema({
     topic: [String],
     post: mongoose.Schema.Types.Mixed,
-    documentSentiment: mongoose.Schema.Types.Mixed,
+    documentSentiment: {
+      score: {
+        type: Number,
+        default: 0
+      },
+      magnitude: {
+        type: Number,
+        default: 0
+      }
+    },
     votes: {
      type: Number,
      default: 0
